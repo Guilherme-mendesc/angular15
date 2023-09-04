ca40a792a27b63086db4f5b9ad017f3cf98eae8a
Guilherme Mendes Carvalho <guimendes1337@gmail.com>
2023-09-04 15:39:40 -0300

    Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,53 +4,19 @@ import { OutletContext } from '@angular/router';
 @Component({
   selector: 'app-root',
   template: `
-  {{valor}}
-  <button (click)="adicionar()">adicionar</button>
-  <app-title *ngIf="destruir"></app-title>
-  <br>
-  <button (click)="destruirComponent()">Destuir componente</button>
+  <app-data-binding></app-data-binding>
   <router-outlet></router-outlet>`
 
 
 
 })
-export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+export class AppComponent implements OnInit {
 
-  public valor: number =1;
-
-  constructor() {}
-
-  public adicionar(): number{
-    return this.valor +=1;
+  constructor(){
   }
 
   ngOnInit(): void {
-  }
-
-  ngDoCheck(): void {
-    console.log("NgDocheck")
-  }
-
-  ngAfterContentInit(): void {
-    console.log("NgContentInit")
-  }
-
-  ngAfterContentChecked(): void {
-    console.log("NgContentChecked")
-  }
-
-  ngAfterViewInit(): void {
-    console.log("NgAfterViewInit")
-  }
-
-  ngAfterViewChecked(): void {
-    console.log("NgAfterViewChecked")
-  }
-  public destruir: boolean = true;
-
-  public destruirComponent(){
-    this.destruir=false;
-
+    
   }
 
   }
