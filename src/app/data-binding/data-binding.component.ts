83e24be87db3ae1eb505ccb9be32f6b670a82f14
Guilherme Mendes Carvalho <guimendes1337@gmail.com>
2023-09-04 15:56:39 -0300

    propertyBinding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -10,7 +10,10 @@ export class DataBindingComponent implements OnInit {
   public nome: string = "Guilherme";
   public idade: number= 27;
   public maisUm: number= 1;
-  
+
+  public checkedDisabled: boolean = false;
+  public imgSrc: string ="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/800px-Unofficial_JavaScript_logo_2.svg.png";
+  public imgTitle: string="Property Binding";
   constructor(){ }
 
   ngOnInit(): void {
